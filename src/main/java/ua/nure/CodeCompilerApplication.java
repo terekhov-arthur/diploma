2bae119849a62f48c56c2e31cdd703640b7cc461
jaffa <jaffalime@gmail.com>
2017-05-02 23:24:31 +0300

    init

---
+++ src/main/java/ua/nure/CodeCompilerApplication.java
@@ -0,0 +1,25 @@
+package ua.nure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+public class CodeCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCompilerApplication.class, args);
+	}
+
+	@Bean
+	public ViewResolver getViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+}
